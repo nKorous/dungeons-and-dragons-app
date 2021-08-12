b9b8fd7e254cc453ec3c0595d0feff93cbef8dbe
nKorous <nathaniel.korous@gmail.com>
2021-08-12 13:08:57 -0600

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,31 @@ import { MonsterFinderComponent } from './monster-finder/monster-finder.componen
 import { MonsterListComponent } from './components/monster-list/monster-list.component';
 import { DxDataGridModule } from 'devextreme-angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
     MonsterFinderComponent,
-    MonsterListComponent
+    MonsterListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     DxDataGridModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
