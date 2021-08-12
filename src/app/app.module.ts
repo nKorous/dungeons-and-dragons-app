d87b3a2004f6313d141ede14166876aee6683e0c
nKorous <nathaniel.korous@gmail.com>
2021-08-12 12:50:59 -0600

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { MonsterFinderComponent } from './monster-finder/monster-finder.component';
 import { MonsterListComponent } from './components/monster-list/monster-list.component';
 import { DxDataGridModule } from 'devextreme-angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { DxDataGridModule } from 'devextreme-angular';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    DxDataGridModule
+    DxDataGridModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
